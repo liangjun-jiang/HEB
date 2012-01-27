fd038701edb8cf514583ed331bd25dd0f3097c4e
Liangjun Jiang <ljiang@MBA.local>
2012-01-26 19:33:34 -0600

    first commit

---
+++ GooglePlaceAPIKey.h
@@ -0,0 +1,15 @@
+//
+//  GooglePlaceAPIKey.h
+//  HEBTest
+//
+//  Created by Liangjun Jiang on 1/15/12.
+//  Copyright (c) 2012 Harvard University Extension School. All rights reserved.
+//
+
+#ifndef HEBTest_GooglePlaceAPIKey_h
+#define HEBTest_GooglePlaceAPIKey_h
+
+#define GooglePlaceAPIKey @"AIzaSyDI9oKyroNMwBTCSWEoSgVfrKtvQ10S3jw"
+
+
+#endif
